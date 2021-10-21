a5f20ec55f4f8043328efb6e84f7384874b38f04
blom112 <42997123+blom112@users.noreply.github.com>
2021-10-21 13:08:02 +0200

    first

--- Client/ClientProgram.cs
+++ Client/ClientProgram.cs
@@ -2,6 +2,7 @@
 using System.Net.Sockets;
 using System.Text;
 using Utillities;
+using System.Text.Json;
 
 namespace Client
 {
@@ -11,17 +12,27 @@ namespace Client
         static void Main(string[] args)
         {
 
+            Request request = new Request();
+            request.Method = "This is the method";
+            request.Date = request.ConvertCurrentDate();
+            request.Body = "This is the body";
+            request.Path = "This is the path";
+
+
+            string output = JsonSerializer.Serialize(request);
+
             var client = new NetworkClient();
 
             client.Connect("localhost", 5000);
 
-            var message = "hello";
+            var message = output;
+
+
             client.Write(message);
 
             var response = client.Read();
 
             Console.WriteLine($"Server response '{response}'");
-
         }
     }
 }
---
+++ Client/Request.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utillities;
+using System.Text.Json;
+
+namespace Client
+{
+    class Request
+    {
+        private string method;
+        public string Method
+        {
+            get { return method; }
+            set { method = value; }
+        }
+
+        private string path;
+        public string Path
+        {
+            get { return path; }
+            set { path = value; }
+        }
+
+        private long date;
+
+         public long Date
+         { 
+             get { return date; }
+            
+             set { date = value; }
+         }
+       
+
+        private string body;
+        public string Body {
+            get { return body; }
+            set { body = value; }
+        }
+
+
+        public long ConvertCurrentDate() {
+
+            DateTime datetime = DateTime.Now; 
+                
+               date = ((DateTimeOffset)datetime).ToUnixTimeSeconds();
+            return date;
+        }
+    }
+ 
+}
